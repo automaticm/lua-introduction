5184c9c86810db57c9036b31c04ab4f0549efff1
Adnan Maksic <adnan_maksic@hotmail.com>
2025-11-30 21:16:47 -0700

    Create init.lua

---
+++ Programs/init.lua
@@ -0,0 +1,30 @@
+valPieces =
+{
+    king = -1,
+    queen = 9,
+    rook = 5,
+    bishop = 3,
+    knight = 3,   
+}
+
+piece = 
+{
+    king = "K",
+    queen = "Q",
+    rook = "R",
+    bishop = "B",
+    knight = "N",
+    pawn = "P",
+}
+
+plr =
+{
+    white = "White",
+    black = "Black"
+}
+
+files = {'a', 'b', 'c', 'd', 'e', 'f', 'g', 'h'}
+ranks = {'1', '2', '3', '4', '5', '6', '7', '8'}
+
+board = {}
+_board = {}
