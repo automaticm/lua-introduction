d434a00b6c46dc47e55d2d3a8f7bb5b8788bce0c
Adnan Maksic <adnan_maksic@hotmail.com>
2025-11-30 21:15:44 -0700

    Update example.lua

--- Programs/example.lua
+++ Programs/example.lua
@@ -1,71 +1,46 @@
+local functions = require("functions")
+local init = require("init")
 
--- Values of pieces
-local valPieces =
-{
-    king = -1,
-    queen = 9,
-    rook = 5,
-    bishop = 3,
-    knight = 3,   
-}
 
-local piece = 
-{
-    King = "K",
-    Queen = "Q",
-    Rook = "R",
-    Bishop = "B",
-    Knight = "N",
-    Pawn = "P",
-}
 
-local plr =
-{
-    white = "White",
-    black = "Black"
-}
-
-local files = {'a', 'b', 'c', 'd', 'e', 'f', 'g', 'h'}
-local rank = {'1', '2', '3', '4', '5', '6', '7', '8'}
-
-local board = {}
-local _board = {}
-
-for _, rank in ipairs(rank) do
+for _, rank in ipairs(ranks) do
     for _, file in ipairs(files) do
         local square = file .. rank
         _board[square] = {piece = nil, plr = nil}
     end
 end
 
+-- __index is called when trying to read a key that doesn't exist in board
+-- _board[key] stores the actual data (piece + plr)
+-- If the key is found in _board, return that
+-- __newIndex is called when you try to write in board.square
+-- only allow assignment if _board isnt board
 setmetatable(board, 
 {
-    -- __index is called when trying to read a key that doesn't exist in board
     __index = function(_, key)
-        -- _board[key] stores the actual data (piece + plr)
-        -- If the key is found in _board, return that
         return _board[key]
     end,
-
-    -- __newIndex is called when you try to write in board.square
     __newindex = function(_, key, value)
-        -- only allow assignment if _board isnt board
         if _board[key] then
             _board[key] = value
+        else
+            print("Error")
         end
     end
 }
 )
 
--- Game functions
+printBoard()
 
-local function placePiece(square, piece, plr)
-    board[square].piece = piece
-    board[square].player = plr
+-- Make function later
+-- Setup the board (pawns only right now)
+for i, file in ipairs(files) do
+    placePiece(file.."2", piece.pawn, plr.black)
 end
 
-local function printBoard()
-    for i = #ranks, 1, -1 do
-        
-    end
+for i, file in ipairs(files) do
+    placePiece(file.."7", piece.pawn, plr.white)
 end
+
+print(" ")
+printBoard()
