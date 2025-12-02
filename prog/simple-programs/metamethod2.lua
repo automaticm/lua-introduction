61f6f0f10d044b80b97570c03350aa540f6edeb9
Ado <adnanmaksic@u.boisestate.edu>
2025-12-01 19:58:06 -0700

    Update

--- prog/simple-programs/metamethod2.lua
+++ prog/simple-programs/metamethod2.lua
@@ -1,1 +1,15 @@
 -- example make a table callable using __call metamethod
+local groot = {}
+
+setmetatable(groot, {
+    __call = function(self, ...)
+        local args = {...}
+        local len = #args
+
+        if len == 0 then
+            return "No one is groot"
+        elseif len == 1 then
+            return "I am groot"
+        end
+    end
+})
